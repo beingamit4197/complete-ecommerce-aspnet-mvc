cef618fcfbbceb5960fdd8f96243199510a8297e
beingamit4197 <beingamit4197@gmail.com>
2022-10-03 11:09:52 +0530

    Added Async Methods in Base Repository

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -23,6 +23,7 @@ namespace eTickets.Controllers
             return View(data);
         }
 
+        //Get: Actors/Edit/1
         public async Task<IActionResult> Edit(int id)
         {
             var actorDetails = await _service.GetByIdAsync(id);
@@ -42,6 +43,29 @@ namespace eTickets.Controllers
             return RedirectToAction(nameof(Index));
 
         }
+
+        // Get: Actors/Delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+
+            if (actorDetails == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
 
         public async Task<IActionResult> Details(int id)
         {
--- eTickets/Data/Services/ActorsService.cs
+++ eTickets/Data/Services/ActorsService.cs
@@ -22,21 +22,11 @@ namespace eTickets.Data.Services
             await _context.SaveChangesAsync();
         }
 
-        public void Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<IEnumerable<Actor>> GetAllAsync()
-        {
-            var result = await _context.Actors.ToListAsync();
-            return result;
-        }
-
-        public async Task<Actor> GetByIdAsync(int id)
+        public async Task DeleteAsync(int id)
         {
             var result = await _context.Actors.FirstOrDefaultAsync(n => n.Id == id);
-            return result;
+            _context.Actors.Remove(result);
+            await _context.SaveChangesAsync();
         }
 
         public async Task <Actor> UpdateAsync(int id, Actor newActor)
