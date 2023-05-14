061b8e48a1158a1c35112c801da120eff35c61c6
Amit Rawat <beingamit4197@gmail.com>
2023-05-14 12:07:04 +0530

    Adding OrdersController file

---
+++ eTickets/Controllers/OrdersController.cs
@@ -0,0 +1,38 @@
+﻿using eTickets.Data.Cart;
+using eTickets.Data.Services;
+using eTickets.Data.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IMoviesService _moviesService;
+        private readonly ShoppingCart _shoppingCart;
+        private readonly IOrdersService _ordersService;
+        public OrdersController(IMoviesService moviesService, ShoppingCart shoppingCart, IOrdersService ordersService)
+        {
+            _moviesService = moviesService;
+            _shoppingCart = shoppingCart;
+            _ordersService = ordersService;
+        }
+        public IActionResult ShoppingCart()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(response);
+        }
+
+    }
+}
